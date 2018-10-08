36474c26769d7c502f9c14b9a2f81bcf51bc4e1d
sivakrishna7 <sivakrishna.karasala@eku.edu>
2018-10-08 08:55:43 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>Aum Namah Shivaya</p>
+          <p>Hara Hara Mahadeva</p>
+          <p>Shambho</p>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
